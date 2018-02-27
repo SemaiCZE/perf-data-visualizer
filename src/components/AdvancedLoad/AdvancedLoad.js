014ce79028f2b4439e20433b633213e4e28cf98f
Petr Stefan <ptr.stef@gmail.com>
2018-02-27 23:29:47 +0100

    Advanced load - since date

--- src/components/AdvancedLoad/AdvancedLoad.js
+++ src/components/AdvancedLoad/AdvancedLoad.js
@@ -9,23 +9,36 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
+import Datetime from 'react-datetime';
 
-const defaultState = { selectedTest: '', loadLast: 10 };
+import './AdvancedLoad.css';
+import { LoadingIcon } from '../../icons';
+
+const defaultState = {
+  selectedTest: '',
+  loadLast: 10,
+  dateFrom: new Date(),
+  isLoading: false
+};
 
 class AdvancedLoad extends Component {
   state = defaultState;
 
   onModalClose() {
     const { onClose } = this.props;
-    this.setState(defaultState);
     onClose();
+    this.setState(defaultState);
+  }
+
+  setLoading() {
+    return Promise.resolve(this.setState({ isLoading: true }));
   }
 
   render() {
     const { isOpen, tests, fetchVersions, fetchValues } = this.props;
 
     return (
-      <Modal show={isOpen} onHide={() => this.onModalClose()}>
+      <Modal show={isOpen} onHide={() => this.onModalClose()} bsSize="large">
         <Modal.Header closeButton>
           <Modal.Title>Advanced Load</Modal.Title>
         </Modal.Header>
@@ -45,77 +58,124 @@ class AdvancedLoad extends Component {
               ))}
             </FormControl>
           </FormGroup>
-          <Row>
-            <Col xs={4} className="text-center" />
-            <Col xs={4} className="text-center">
-              <FormGroup>
-                <ControlLabel>N:</ControlLabel>
-                &nbsp;
-                <FormControl
-                  componentClass="input"
-                  type="number"
-                  min="1"
-                  defaultValue={this.state.loadLast}
-                  onChange={e =>
-                    this.setState({ loadLast: Number(e.target.value) })
-                  }
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={4} className="text-center">
-              <Button
-                bsStyle="primary"
-                disabled={this.state.selectedTest === ''}
-                onClick={() =>
-                  fetchVersions(this.state.selectedTest)
-                    .then(versions =>
-                      Promise.all(
-                        versions.map(version =>
-                          fetchValues(this.state.selectedTest, version.id)
+          <hr />
+          {!this.state.isLoading && (
+            <div>
+              <Row className="formRow">
+                <Col xs={4} className="text-center" />
+                <Col xs={4} className="text-center">
+                  <FormGroup>
+                    <Col sm={2}>
+                      <ControlLabel>N:</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <FormControl
+                        componentClass="input"
+                        type="number"
+                        min="1"
+                        defaultValue={this.state.loadLast}
+                        onChange={e =>
+                          this.setState({ loadLast: Number(e.target.value) })
+                        }
+                      />
+                    </Col>
+                  </FormGroup>
+                </Col>
+                <Col xs={4} className="text-center">
+                  <FormGroup>
+                    <Col sm={2}>
+                      <ControlLabel>X:</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <Datetime
+                        utc={true}
+                        defaultValue={this.state.dateFrom}
+                        onChange={value =>
+                          this.setState({ dateFrom: value.unix() })
+                        }
+                      />
+                    </Col>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={4} className="text-center">
+                  <Button
+                    bsStyle="primary"
+                    disabled={this.state.selectedTest === ''}
+                    onClick={() =>
+                      this.setLoading()
+                        .then(() => fetchVersions(this.state.selectedTest))
+                        .then(versions =>
+                          Promise.all(
+                            versions.map(version =>
+                              fetchValues(this.state.selectedTest, version.id)
+                            )
+                          )
                         )
-                      )
-                    )
-                    .then(() => this.onModalClose())
-                }
-              >
-                Load all versions
-              </Button>
-            </Col>
-            <Col xs={4} className="text-center">
-              <Button
-                bsStyle="primary"
-                disabled={this.state.selectedTest === ''}
-                onClick={() =>
-                  fetchVersions(this.state.selectedTest)
-                    .then(versions =>
-                      Promise.all(
-                        versions
-                          .filter(
-                            (item, i, arr) =>
-                              i + this.state.loadLast >= arr.length
+                        .then(() => this.onModalClose())
+                    }
+                  >
+                    Load all versions
+                  </Button>
+                </Col>
+                <Col xs={4} className="text-center">
+                  <Button
+                    bsStyle="primary"
+                    disabled={this.state.selectedTest === ''}
+                    onClick={() =>
+                      this.setLoading()
+                        .then(() => fetchVersions(this.state.selectedTest))
+                        .then(versions =>
+                          Promise.all(
+                            versions
+                              .filter(
+                                (item, i, arr) =>
+                                  i + this.state.loadLast >= arr.length
+                              )
+                              .map(version =>
+                                fetchValues(this.state.selectedTest, version.id)
+                              )
                           )
-                          .map(version =>
-                            fetchValues(this.state.selectedTest, version.id)
+                        )
+                        .then(() => this.onModalClose())
+                    }
+                  >
+                    Load last N
+                  </Button>
+                </Col>
+                <Col xs={4} className="text-center">
+                  <Button
+                    bsStyle="primary"
+                    disabled={this.state.selectedTest === ''}
+                    onClick={() =>
+                      this.setLoading()
+                        .then(() => fetchVersions(this.state.selectedTest))
+                        .then(versions =>
+                          Promise.all(
+                            versions
+                              .filter(
+                                item => item.timestamp >= this.state.dateFrom
+                              )
+                              .map(version =>
+                                fetchValues(this.state.selectedTest, version.id)
+                              )
                           )
-                      )
-                    )
-                    .then(() => this.onModalClose())
-                }
-              >
-                Load last N
-              </Button>
-            </Col>
-            <Col xs={4} className="text-center">
-              <Button
-                bsStyle="primary"
-                disabled={this.state.selectedTest === ''}
-              >
-                Load newer than X
-              </Button>
-            </Col>
-          </Row>
+                        )
+                        .then(() => this.onModalClose())
+                    }
+                  >
+                    Load newer than X
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          )}
+          {this.state.isLoading && (
+            <Row className="text-center">
+              <LoadingIcon className="loadingIcon" />
+            </Row>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={() => this.onModalClose()}>Close</Button>
